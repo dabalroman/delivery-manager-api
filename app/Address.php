e02dc03988d580d302ee153c375979e10d53bc21
Roman Dąbal <dabalroman@gmail.com>
2020-05-28 02:06:37 +0200

    Ide-helper for Models

--- app/Address.php
+++ app/Address.php
@@ -2,8 +2,49 @@
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Address
+ *
+ * @property int $id
+ * @property string $city
+ * @property string $street
+ * @property string $street_number
+ * @property string $flat_number
+ * @property int $floor
+ * @property string $client_name
+ * @property string $delivery_hours
+ * @property string $phone
+ * @property string $code
+ * @property string $comment
+ * @property string $geo_cord
+ * @property string $id_hash
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Address newModelQuery()
+ * @method static Builder|Address newQuery()
+ * @method static Builder|Address query()
+ * @method static Builder|Address whereCity($value)
+ * @method static Builder|Address whereClientName($value)
+ * @method static Builder|Address whereCode($value)
+ * @method static Builder|Address whereComment($value)
+ * @method static Builder|Address whereCreatedAt($value)
+ * @method static Builder|Address whereDeliveryHours($value)
+ * @method static Builder|Address whereFlatNumber($value)
+ * @method static Builder|Address whereFloor($value)
+ * @method static Builder|Address whereGeoCord($value)
+ * @method static Builder|Address whereId($value)
+ * @method static Builder|Address whereIdHash($value)
+ * @method static Builder|Address wherePhone($value)
+ * @method static Builder|Address whereStreet($value)
+ * @method static Builder|Address whereStreetNumber($value)
+ * @method static Builder|Address whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Address extends Model
 {
     protected $table = 'address';
--- app/User.php
+++ app/User.php
@@ -2,12 +2,22 @@
 
 namespace App;
 
+use Eloquent;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
+/**
+ * App\User
+ *
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @mixin Eloquent
+ */
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
